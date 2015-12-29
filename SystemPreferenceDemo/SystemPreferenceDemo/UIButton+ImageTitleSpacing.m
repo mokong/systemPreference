69df9b5ae5927ccdca17a01f69bac6848b7eed66
mokong <a525325614@163.com>
2015-12-29 11:40:12 +0800

    1. 添加完整的Category

--- SystemPreferenceDemo/SystemPreferenceDemo/UIButton+ImageTitleSpacing.m
+++ SystemPreferenceDemo/SystemPreferenceDemo/UIButton+ImageTitleSpacing.m
@@ -10,6 +10,63 @@
 
 @implementation UIButton (ImageTitleSpacing)
 
+- (void)layoutButtonWithEdgeInsetsStyle:(MKButtonEdgeInsetsStyle)style
+                        imageTitleSpace:(CGFloat)space
+{
+    // 1. 得到imageView和titleLabel的宽、高
+    CGFloat imageWith = self.imageView.frame.size.width;
+    CGFloat imageHeight = self.imageView.frame.size.height;
+    
+    CGFloat labelWidth = 0.0;
+    CGFloat labelHeight = 0.0;
+    if ([UIDevice currentDevice].systemVersion.floatValue >= 8.0) {
+        // 由于iOS8中titleLabel的size为0，用下面的这种设置
+        labelWidth = self.titleLabel.intrinsicContentSize.width;
+        labelHeight = self.titleLabel.intrinsicContentSize.height;
+    } else {
+        labelWidth = self.titleLabel.frame.size.width;
+        labelHeight = self.titleLabel.frame.size.height;
+    }
+    
+    // 2. 声明全局的imageEdgeInsets和labelEdgeInsets
+    UIEdgeInsets imageEdgeInsets = UIEdgeInsetsZero;
+    UIEdgeInsets labelEdgeInsets = UIEdgeInsetsZero;
+    
+    // 3. 根据style和space得到imageEdgeInsets和labelEdgeInsets的值
+    switch (style) {
+        case MKButtonEdgeInsetsStyleTop:
+        {
+            imageEdgeInsets = UIEdgeInsetsMake(-labelHeight-space/2.0, 0, 0, -labelWidth);
+            labelEdgeInsets = UIEdgeInsetsMake(0, -imageWith, -imageHeight-space/2.0, 0);
+        }
+            break;
+        case MKButtonEdgeInsetsStyleLeft:
+        {
+            imageEdgeInsets = UIEdgeInsetsMake(0, -space/2.0, 0, -space/2.0);
+            labelEdgeInsets = UIEdgeInsetsMake(0, space/2.0, 0, -space/2.0);
+        }
+            break;
+        case MKButtonEdgeInsetsStyleBottom:
+        {
+            imageEdgeInsets = UIEdgeInsetsMake(labelHeight+space/2.0, 0, 0, -labelWidth);
+            labelEdgeInsets = UIEdgeInsetsMake(0, -imageWith, imageHeight+space/2.0, 0);
+        }
+            break;
+        case MKButtonEdgeInsetsStyleRight:
+        {
+            imageEdgeInsets = UIEdgeInsetsMake(0, space/2.0, 0, space/2.0);
+            labelEdgeInsets = UIEdgeInsetsMake(0, -space/2.0, 0, space/2.0);
+        }
+            break;
+        default:
+            break;
+    }
+    
+    // 4. 赋值
+    self.titleEdgeInsets = labelEdgeInsets;
+    self.imageEdgeInsets = imageEdgeInsets;
+}
+
 - (void)centerButtonAndImageWithSpacing:(CGFloat)spacing {
 
     /**
@@ -18,6 +75,7 @@
     
     UIEdgeInsets imageEdgeInsets = UIEdgeInsetsZero;
     if ([UIDevice currentDevice].systemVersion.floatValue >= 8.0) {
+        // 由于iOS8中titleLabel的size为0，用上面这样设置有问题，修改一下即可
         imageEdgeInsets = UIEdgeInsetsMake(-self.titleLabel.intrinsicContentSize.height-spacing/2.0, 0, 0, -self.titleLabel.intrinsicContentSize.width);
     } else {
         imageEdgeInsets = UIEdgeInsetsMake(-self.titleLabel.frame.size.height-spacing/2.0, 0, 0, -self.titleLabel.frame.size.width);
